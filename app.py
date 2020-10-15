4c5f0583e6dd6ea09974a30702e0cd9638e427d0
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-15 16:52:50 +0100

    Day 4 Lab 2 Completed to Extension 3

---
+++ app.py
@@ -0,0 +1,33 @@
+from modules.output import *
+from data.task_list import *
+from modules.input import *
+
+while (True):
+    print_menu()
+    option = get_option_choice()
+    if (option.lower() == 'q'):
+        break
+    if option == '1':
+        print_list(tasks)
+    elif option == '2':
+        print_list(get_uncompleted_tasks(tasks))
+    elif option == '3':
+        print_list(get_completed_tasks(tasks))
+    elif option == '4':
+        description = get_task_description()
+        task = get_task_with_description(tasks, description)
+        if task != "Task Not Found":
+            mark_task_complete(task)
+    elif option == '5':
+        time = get_duration()
+        print_list(get_tasks_taking_longer_than(tasks, time))
+    elif option == '6':
+        description = get_task_search()
+        print(get_task_with_description(tasks, description))
+    elif option == '7':
+        description = input("Enter description: ")
+        time_taken = int(input("Enter time taken: "))
+        task = create_task(description, time_taken)
+        tasks.append(task)
+    else:
+        print("Invalid Input - choose another option")
---
+++ modules/input.py
@@ -0,0 +1,19 @@
+def get_option_choice():
+    print()
+    option = input("Select an option 1, 2, 3, 4, 5 or (Q)uit: ")
+    return option
+
+def get_task_description():
+    print()
+    description = input("Enter task description to search for: ")
+    return description
+
+def get_duration():
+    print()
+    duration = int(input("Enter task duration: "))
+    return duration
+
+def get_task_search():
+    print()
+    task_search = input("Enter task description to search for: ")
+    return task_search
